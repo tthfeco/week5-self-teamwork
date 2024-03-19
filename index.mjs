bb4819d717028441ed7ad3616b1acb99d493ffa0
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2024-03-19 10:17:41 +0100

    create arrays

---
+++ index.mjs
@@ -0,0 +1,5 @@
+let nouns = ['Hammer', 'Drill', 'House', 'Rose', 'Banana', 'Lamp'];
+let verbs = ['Help', 'Run', 'Play', 'Cook', 'Read', 'Write'];
+let adjectives = ['Colorful', 'Black', 'Dark', 'Hard', 'Soft'];
+let names = ['Spiderman', 'Frodo', 'Batman', 'Grinch', 'Trump', 'Clinton'];
+
