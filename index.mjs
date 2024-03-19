2f7da7b1f78758a37b85ac50aa85d52e3d36ba7f
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2024-03-19 16:53:19 +0100

    prepare for export

--- index.mjs
+++ index.mjs
@@ -3,3 +3,4 @@ let verbs = ['Help', 'Run', 'Play', 'Cook', 'Read', 'Write'];
 let adjectives = ['Colorful', 'Black', 'Dark', 'Hard', 'Soft'];
 let names = ['Spiderman', 'Frodo', 'Batman', 'Grinch', 'Trump', 'Clinton'];
 
+export {nouns, verbs, adjectives, names};
