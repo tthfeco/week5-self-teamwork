22e3562cc39eba9de1de1478012b4a80de04371a
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2024-03-19 17:20:51 +0100

    create 3 random name with variable

--- index.mjs
+++ index.mjs
@@ -6,6 +6,10 @@ let names = ['Spiderman', 'Frodo', 'Batman', 'Grinch', 'Trump', 'Clinton'];
 const allWords = nouns.concat(verbs, adjectives, names);
 const randomIndex = Math.floor(Math.random() * allWords.length);
 
+const randomAdjective = Math.floor(Math.random() * adjectives.length);
+const randomConjuction = Math.floor(Math.random() * conjuctions.length);
+const randomAdjective2 = Math.floor(Math.random() * adjectives.length);
+
 export { nouns, verbs, adjectives, names };
 
 import { brandNameGenerator } from './bandName.mjs';
