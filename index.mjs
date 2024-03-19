914db4fbf1f0e5498c454c9429fc615a243f1e3d
Toth Ferenc <150048021+tthfeco@users.noreply.github.com>
2024-03-19 17:16:31 +0100

    move the vaiable and the import-export snippets

--- index.mjs
+++ index.mjs
@@ -1,3 +1,9 @@
+export { nouns, verbs, adjectives, names };
+
+import { brandNameGenerator } from './bandName.mjs';
+import { albumTitleGenerator } from './albumTitleGen.mjs';
+
+
 let nouns = ['Hammer', 'Drill', 'House', 'Rose', 'Banana', 'Lamp'];
 let verbs = ['Help', 'Run', 'Play', 'Cook', 'Read', 'Write'];
 let adjectives = ['Colorful', 'Black', 'Dark', 'Hard', 'Soft'];
@@ -9,10 +15,6 @@ const randomIndex = Math.floor(Math.random() * allWords.length);
 const randomTitle = Math.floor(Math.random()* verbs.length);
 const randomTitle2 = Math.floor(Math.random()* nouns.length);
 
-export { nouns, verbs, adjectives, names };
-
-import { brandNameGenerator } from './bandName.mjs';
-import { albumTitleGenerator } from './albumTitleGen.mjs';
 
 function main() {
   const createdBandName = brandNameGenerator(allWords[randomIndex], allWords);
